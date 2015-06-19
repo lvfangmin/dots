402cd7a64ba4d49d6ac7337308bf4e477b035ad4
lvfangmin <lvfangmin@gmail.com>
2015-06-19 17:44:40 +0800

    add specific initialization part for bundles

--- init_vim.sh
+++ init_vim.sh
@@ -8,6 +8,7 @@ mkdir -p "$VIM/autoload/" "$VIM/bundle/"
 # install pathogen to manage vim plugins
 curl -LSso $VIM/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
+# download all bundles
 BUNDLES=("git@github.com:mileszs/ack.vim.git"
          "git@github.com:vim-scripts/AutoComplPop.git"
          "git@github.com:wincent/command-t.git"
@@ -34,3 +35,23 @@ for bundle in "${BUNDLES[@]}"; do
 done
 
 popd
+
+# init command-t
+if [ -e $VIM/bundle/command-t/ruby/command-t ]; then
+  pushd . && cd $VIM/bundle/command-t/ruby/command-t
+  ruby extconf.rb && make && make install
+  popd
+fi
+
+# init tagbar
+pushd . && cd /tmp
+# download and install tagbar's prerequest ctags
+CTAGS_URL=http://ncu.dl.sourceforge.net/project/ctags/ctags/5.8/ctags-5.8.tar.gz
+curl -LSso ctags.tar.gz $CTAGS_URL \
+  && tar -zxvf ctags.tar.gz && cd ctags && ./configure && make \
+  && sudo make install
+# clean up
+cd .. && rm -rf ctags && rm ctags.tar.gz
+popd
+
+# init power line
